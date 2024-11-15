2baf6ea459142e3d2a5a931599841bb1f727e901
agnieszka <agnieszka.pisarek@klosebrothers.com>
2024-11-15 14:53:34 +0100

    add Create Task component

--- Vue3App/vite-project/src/App.vue
+++ Vue3App/vite-project/src/App.vue
@@ -1,9 +1,10 @@
 <script>
 import RandomText from './components/RandomText.vue'
-
+import CreateTask from './components/CreateTask.vue'
+import Task from './components/Task.vue'
 export default {
   name: 'App',
-  components: { RandomText },
+  components: { Task, RandomText, CreateTask },
   data() {
     return {
       encouragement: 'Manage your tasks and stay productive...',
@@ -17,6 +18,11 @@ export default {
     <section class="greeting">
       <RandomText />
       <div class="encouragement">{{ encouragement }}</div>
+      <Task />
+    </section>
+
+    <section>
+      <CreateTask />
     </section>
   </main>
 </template>
---
+++ Vue3App/vite-project/src/components/Task.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section class="task">
+    <div class="task-container">
+      <button class="checkButton" @click="handleClick"></button>
+      <input class="thingToDo" type="text" v-model="input" />
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.task-container {
+  margin-top: 100px;
+  width: 451px;
+  height: 36px;
+  gap: 10px;
+  border-radius: 12px;
+  background: #e6e6e6;
+  padding: 10px;
+  border: 0 solid #ccc;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.15);
+  transition:
+    box-shadow 0.3s ease,
+    border-color 0.3s ease;
+}
+.checkButton {
+  width: 18px;
+  height: 18px;
+  background-color: #ededed;
+  border: 0 solid #ccc;
+  border-radius: 6px;
+}
+
+.thingToDo {
+  font-size: 16px;
+  font-weight: 500;
+  color: #494955;
+}
+</style>
