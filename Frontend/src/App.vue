01f7819396147e46078558906ddbc74c1d5b98e5
agnieszka <agnieszka.pisarek@klosebrothers.com>
2024-11-28 10:55:24 +0100

    add encouragement

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
 
 import RandomText from '@/components/RandomText.vue'
+
+const encouragement = 'Manage your tasks and stay productive...'
 </script>
 
 <template>
   <main class="App">
     <section class="greeting">
       <RandomText />
-<!--&lt;!&ndash;      <div class="encouragement">{{ encouragement }}</div>&ndash;&gt;-->
+      <div class="encouragement">{{ encouragement }}</div>
 <!--      <Task />-->
 <!--    </section>-->
 
