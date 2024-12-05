ae3151ed4b90618b09a212f4235d7650c0ee65b9
agnieszka <agnieszka.pisarek@klosebrothers.com>
2024-12-05 11:11:49 +0100

    text is crossed out text when the task is marked as completed

--- Frontend/src/components/Task.vue
+++ Frontend/src/components/Task.vue
@@ -2,21 +2,32 @@
 import { ref } from 'vue'
 import OptionsMenu from '@/components/OptionsMenu.vue'
 const input = ref('')
+const isChecked = ref(false)
+const task = ref<HTMLInputElement | null>(null)
 
 const checkButton = document.querySelector('.checkButton')
-
 if (checkButton) {
   checkButton.addEventListener('click', () => {
     checkButton.classList.add('taskDoneButton')
   })
 }
+
+function clickCheck() {
+  isChecked.value = !isChecked.value;
+  if(isChecked.value && task.value) {
+    task.value.style.textDecoration = "line-through";
+  } else if(task.value) {
+    task.value.style.textDecoration = "none";
+  }
+}
+
 </script>
 
 <template>
   <section class="task">
     <div class="task-container">
-      <input id="checkButton" type="checkbox"></input>
-      <input class="thingToDo" type="text" v-model="input" />
+      <input id="checkButton" type="checkbox" @click="clickCheck"></input>
+      <input class="thingToDo" type="text" v-model="input" ref="task"/>
       <input class="date" type="date" />
       <OptionsMenu />
     </div>
@@ -73,6 +84,7 @@ if (checkButton) {
   background: #ffffff;
   padding: 10px;
   border: 0 solid #ccc;
+  text-decoration: none;
   transition:
     box-shadow 0.3s ease,
     border-color 0.3s ease;
