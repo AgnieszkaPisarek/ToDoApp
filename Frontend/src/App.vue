f33db6e0fb629d27aac286370ef2a2f7e7de0364
agnieszka <agnieszka.pisarek@klosebrothers.com>
2024-12-06 10:03:39 +0100

    adding empty tasks

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -4,18 +4,19 @@ import Task from '@/components/Task.vue'
 import CreateTask from '@/components/CreateTask.vue'
 import { OhVueIcon, addIcons } from 'oh-vue-icons'
 import { IoEyeOutline } from 'oh-vue-icons/icons'
-import { createApp } from 'vue'
+import { ref } from 'vue'
 import App from '@/App.vue'
 
 const encouragement = 'Manage your tasks and stay productive...'
+const tasks = ref(['task1', 'task2', 'task3', 'task4'])
 // addIcons(IoEyeOutline)
 // const app = createApp(App)
 // app.component('v-icon', OhVueIcon)
 // app.mount('#app')
 
-function deleteTask(id: number) {}
-const handleAddTask = () => {
-  console.log('Dziala')
+const handleAddTask = (task: string) => {
+  console.log(task)
+  tasks.value.push('task' + tasks.value.length)
 }
 </script>
 
@@ -24,13 +25,7 @@ const handleAddTask = () => {
     <section class="greeting">
       <RandomText />
       <div class="encouragement">{{ encouragement }}</div>
-      <div class="tasksField">
-        <Task />
-        <Task />
-        <Task />
-        <Task />
-      </div>
-
+      <Task v-for="(task, index) in tasks" :key="index" :task="task"> </Task>
       <CreateTask @addTaskEvent="handleAddTask" />
       <v-icon name="IoEyeOutline" />
       <div class="completed">Completed 0 of 4</div>
@@ -44,11 +39,7 @@ const handleAddTask = () => {
   flex-direction: column;
   align-items: center;
 }
-.tasksField {
-  height: 500px;
-  width: auto;
-  overflow: visible;
-}
+
 .encouragement {
   font-size: 18px;
 }
