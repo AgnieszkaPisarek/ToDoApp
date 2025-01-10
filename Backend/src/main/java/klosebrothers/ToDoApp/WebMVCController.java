836b020dfcd987c3584fb965fd9638db8e16ae1f
agnieszka <agnieszka.pisarek@klosebrothers.com>
2025-01-10 13:34:56 +0100

    add addition, change and deletion of the task in an arraylist via Controller

--- Backend/src/main/java/klosebrothers/ToDoApp/WebMVCController.java
+++ Backend/src/main/java/klosebrothers/ToDoApp/WebMVCController.java
@@ -1,40 +1,49 @@
 package klosebrothers.ToDoApp;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Controller
+@RestController
 public class WebMVCController {
 
-    @GetMapping("/tasks")
-    @ResponseBody
-    public List<Task> returnTasks() {
-        List<Task> tasks = new ArrayList<>();
+    private static final List<Task> tasks;
+
+    static {
+        tasks = new ArrayList<>();
         tasks.add(new Task("1", "Take a dog for a walk", "2025-01-03", true));
         tasks.add(new Task("2", "Take out the trash", "2025-01-04", false));
         tasks.add(new Task("3", "Do the dishes", "2025-01-05", false));
         tasks.add(new Task("4", "Updated task title", "2025-01-07", true));
+    }
 
+    @GetMapping("/tasks")
+    @ResponseBody
+    public List<Task> returnTasks() {
         return tasks;
     }
 
-    @DeleteMapping
-    public void deleteTask(@RequestParam("id") int id) {
-
+    @DeleteMapping("/tasks/{id}")
+    public void deleteTask(@PathVariable("id")String id) {
+        tasks.removeIf(task -> task.getId().equals(id));
     }
 
-    @PostMapping
-    public void addTask(@ModelAttribute Task task) {
-
+    @PostMapping("/tasks")
+    public void addTask(@RequestBody Task newTask) {
+        tasks.add(newTask);
     }
 
-    @PatchMapping
-    public void updateTask(@ModelAttribute Task task) {
-
+    @PutMapping("/tasks/{id}")
+    public void updateTask(@RequestBody Task changedTask, @PathVariable String id) {
+        tasks.stream()
+                .filter(task -> task.getId().equals(id))
+                .findFirst()
+                .ifPresent(task -> {
+                    task.setDescription(changedTask.getDescription());
+                    task.setDate(changedTask.getDate());
+                    task.setCompleted(changedTask.isCompleted());
+                });
     }
-
 }
