698342d89a8a7e9d6b174e6bd6120b10b729a3ec
agnieszka <agnieszka.pisarek@klosebrothers.com>
2025-01-10 08:31:22 +0100

    refactor cypress tests

--- Frontend/cypress/e2e/ToDoApp.cy.ts
+++ Frontend/cypress/e2e/ToDoApp.cy.ts
@@ -2,6 +2,7 @@ describe('Accessing the page', () => {
   beforeEach(() => {
     cy.visit('/')
   })
+
   const texts = [
     '👋 Hello, Energizer!',
     "G'Day Boss!",
@@ -9,6 +10,7 @@ describe('Accessing the page', () => {
     'Keep it going!',
     'Hello sunshine! ☀️',
   ]
+
   it('should validate all static text on the page', () => {
     cy.get('.encouragement').should(
       'contain.text',
@@ -22,64 +24,110 @@ describe('Accessing the page', () => {
   })
 })
 
-describe('Creating tasks', () => {
+describe('Creating and deleting tasks', () => {
+
+  type Task = {
+    id: string
+    description: string
+    date: string
+    completed: boolean
+  }
+
+  let numberOfTasks = 0
+  let numberOfCompletedTasks = 0
+
   before(() => {
+    cy.intercept('GET', '/tasks').as('getData')
+    cy.visit('/')
+    cy.wait('@getData').then((interception) => {
+      const responseData = interception.response?.body
+      numberOfTasks = responseData.length
+      responseData.forEach((task: Task) => {
+            if (task.completed) {
+              numberOfCompletedTasks++
+            }
+          })
+      });
+  })
+
+  beforeEach(() => {
     cy.visit('/')
   })
-  it('should successfully create many tasks', () => {
-    cy.get('.task').should('not.exist')
+
+  it('should successfully create many tasks and then delete them', () => {
 
     cy.get('.createTaskInput').first().type('Wash the dishes')
     cy.get('.addTaskButton').click()
-    cy.get('.task').should('exist')
-    cy.get('.thingToDo').eq(0).should('have.value', 'Wash the dishes')
-    cy.get('.completedText').should('contain.text', 'Completed 0 of 1')
+    cy.get('.thingToDo').eq(numberOfTasks).should('have.value', 'Wash the dishes')
+    cy.get('.completedText').should('contain.text', 'Completed ' + numberOfCompletedTasks + ' of ' + ++numberOfTasks)
 
     cy.get('.createTaskInput').type('Take out the trash')
     cy.get('.addTaskButton').first().click()
-    cy.get('.thingToDo').eq(1).should('have.value', 'Take out the trash')
-    cy.get('.completedText').should('contain.text', 'Completed 0 of 2')
+    cy.get('.thingToDo').eq(numberOfTasks).should('have.value', 'Take out the trash')
+    cy.get('.completedText').should('contain.text', 'Completed ' + numberOfCompletedTasks + ' of ' + ++numberOfTasks)
 
     cy.get('.createTaskInput').type('Take the dog for a walk')
     cy.get('.addTaskButton').first().click()
-    cy.get('.thingToDo').eq(2).should('have.value', 'Take the dog for a walk')
-    cy.get('.completedText').should('contain.text', 'Completed 0 of 3')
+    cy.get('.thingToDo').eq(numberOfTasks).should('have.value', 'Take the dog for a walk')
+    cy.get('.completedText').should('contain.text', 'Completed ' + numberOfCompletedTasks + ' of ' + ++numberOfTasks)
 
     cy.get('.createTaskInput').type('Do the homework')
     cy.get('.addTaskButton').first().click()
-    cy.get('.thingToDo').eq(3).should('have.value', 'Do the homework')
-    cy.get('.completedText').should('contain.text', 'Completed 0 of 4')
-    cy.get('.thingToDo').should('have.length', 4)
-  })
-})
-
-describe('Editing and deleting tasks', () => {
-  beforeEach(() => {
-    cy.visit('/')
-    cy.get('.createTaskInput').type('Mow the lawn')
-    cy.get('.addTaskButton').click()
-    cy.get('.createTaskInput').type('Feed the dog')
-    cy.get('.addTaskButton').click()
-  })
-  it('should visibly edit second task', () => {
-    cy.get('.thingToDo').eq(1).should('have.value', 'Feed the dog')
-    cy.get('input.thingToDo').eq(1).clear()
-    cy.get('input.thingToDo').eq(1).type('Feed the cat')
-    cy.get('.thingToDo').eq(1).should('have.value', 'Feed the cat')
-    cy.get('.date').eq(1).type('2012-12-12')
-    cy.get('.date').eq(1).should('have.value', '2012-12-12')
+    cy.get('.thingToDo').eq(numberOfTasks).should('have.value', 'Do the homework')
+    cy.get('.completedText').should('contain.text', 'Completed ' + numberOfCompletedTasks + ' of ' + ++numberOfTasks)
+    cy.get('.thingToDo').should('have.length', numberOfTasks)
   })
 
-  it('should delete given task', () => {
-    cy.get('.completedText').should('contain.text', 'Completed 0 of 2')
+  it('should successfully delete tasks', () => {
+
     cy.get('.options-menu').should('not.exist')
-    cy.get('.menu-sign').eq(1).click()
+    cy.get('.menu-sign').eq(--numberOfTasks).click()
     cy.get('.options-menu').should('be.visible')
     cy.get('.options-menu').click()
 
+    cy.get('.completedText').should('contain.text', 'Completed ' + numberOfCompletedTasks + ' of ' + numberOfTasks)
     cy.get('.options-menu').should('not.exist')
-    cy.get('.thingToDo').should('have.length', 1)
-    cy.get('.thingToDo').should('have.value', 'Mow the lawn')
-    cy.get('.completedText').should('contain.text', 'Completed 0 of 1')
+    cy.get('.menu-sign').eq(--numberOfTasks).click()
+    cy.get('.options-menu').should('be.visible')
+    cy.get('.options-menu').click()
+
+    cy.get('.completedText').should('contain.text', 'Completed ' + numberOfCompletedTasks + ' of ' + numberOfTasks)
+    cy.get('.options-menu').should('not.exist')
+    cy.get('.menu-sign').eq(--numberOfTasks).click()
+    cy.get('.options-menu').should('be.visible')
+    cy.get('.options-menu').click()
+
+    cy.get('.completedText').should('contain.text', 'Completed ' + numberOfCompletedTasks + ' of ' + numberOfTasks)
+    cy.get('.options-menu').should('not.exist')
+    cy.get('.menu-sign').eq(--numberOfTasks).click()
+    cy.get('.options-menu').should('be.visible')
+    cy.get('.options-menu').click()
   })
+})
+
+describe('Editing tasks', () => {
+
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+  it('should visibly edit task', () => {
+
+    cy.get('.task').should('exist').then(() => {
+      cy.get('.thingToDo').eq(0).invoke('val').then((previousTaskDescription) => {
+        cy.get('input.thingToDo').eq(0).clear()
+        cy.get('input.thingToDo').eq(0).type('Feed the cat')
+        cy.get('.thingToDo').eq(0).should('have.value', 'Feed the cat')
+        cy.get('input.thingToDo').eq(0).clear().type(String(previousTaskDescription))
+      })
+
+      cy.get('.date').eq(0).invoke('val').then((previousDate) => {
+        cy.get('.date').eq(0).clear()
+        cy.get('.date').eq(0).type('2012-12-12')
+        cy.get('.date').eq(0).should('have.value', '2012-12-12')
+        cy.get('.date').eq(0).clear().type(String(previousDate))
+      })
+    })
+  })
+
 })
